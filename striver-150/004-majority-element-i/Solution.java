b9b27213656bcee55c2793f4a1496ccfc8233aa4
Matías Acuña <matias@matiasacuna.dev>
2026-09-24 12:25:54 -0300

    Majority Element-I

--- striver-150/004-majority-element-i/Solution.java
+++ striver-150/004-majority-element-i/Solution.java
@@ -8,5 +8,31 @@
  */
 
 class Solution {
-    // TODO: implement your own solution following the required signature.
+public int majorityElement(int[] nums) {
+
+       int count = 1;
+       int candidate = nums[0];
+
+       for (int i = 1; i < nums.length; i++){
+
+
+        if (candidate == nums[i]){
+
+            count++;
+
+        } else{
+
+            count--;
+
+            if (count == 0){
+
+                candidate = nums[i];
+                count = 1;
+
+            }
+        
+        }
+       }
+        return candidate;
+    }
 }
